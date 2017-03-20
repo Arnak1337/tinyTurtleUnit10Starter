311d9a5ecec9ec5145dbed62b59a85521d02dc27
Esteban Hernandez <arnak1337@gmail.com>
2017-03-20 14:20:11 +0000

    uh

--- script.js
+++ script.js
@@ -12,7 +12,27 @@ right(90);
 forward(50);
 }
 
+function triangle(){
+    forward(75);
+    right(120);
+    forward(75);
+    right(120);
+    forward(75);
+    right(120);
+}
+
+function rectangle(){
+left(90);
+forward(50);
+left(90);
+forward(70);
+left(90);
+forward(50);
+left(90);
+forward(70);
+}
 // Type your function call below
-
-
+square();
+rectangle();
+triangle();
 stamp();
